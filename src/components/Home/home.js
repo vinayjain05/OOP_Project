c61f6fbf792e027f963f71d34ffa7753e151bd90
Parinav <parinavputhran@gmail.com>
2021-07-13 19:23:43 +0530

    homepage svg added

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import { ReactComponent as HomePicture } from "../../svg/homedocs.svg";
+import styles from "../../css/home.module.css";
 
 export default class Home extends Component {
   render() {
     return (
       <React.Fragment>
-        <div>ashdkjmasd</div>
+        <div className={styles.homeContainer}>
+          <div className={styles.homedocsContainer}>
+            <HomePicture className={styles.landingsvg} />
+          </div>
+          <div className={styles.glassBoard}>
+            <div className={styles.topBoard}></div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
